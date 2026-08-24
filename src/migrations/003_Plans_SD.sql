1bc2563fa1f16237fdddbcf32309268cb7a23b69
Modymanour <modymansour2004@gmail.com>
2026-08-24 18:08:41 +0300

    added the layout services for the system and finished tenants service

---
+++ src/migrations/003_Plans_SD.sql
@@ -0,0 +1,11 @@
+INSERT INTO plans (plan_name,api_call_limit,api_token_limit)
+VALUES ("Free",1000,100000), ("Pro", 10000,1000000);
+
+ALTER TABLE user_events
+ADD CONSTRAINT UNIQUE(tenant_id, idempotency_key);
+
+ALTER TABLE tenants
+ADD email varchar(255) NOT NULL CHECK(LENGTH(email) > 0);
+
+ALTER TABLE tenants
+ADD password varchar(1000) NOT NULL CHECK(LENGTH(password) > 0);
